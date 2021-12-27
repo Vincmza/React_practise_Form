aae96509839682acf00e7722036384cf9c9c84ce
Vincent Manzanera <vincent.mzapro@outlook.fr>
2021-12-27 17:11:55 +0100

    UserInfosForm.js : improved function to display birth date, used Date constructor and toLocalDateString method

--- src/components/User-Infos/UserInfosForm.js
+++ src/components/User-Infos/UserInfosForm.js
@@ -1,57 +1,42 @@
 import { Field, BirthDate, Email, Phone, GitHub } from "./Inputs";
 import React from "react";
 
-function UserInfosForm({userData, setUserData}) {
-	const handleChange = (e)=>{
-		setUserData(pv=>({...pv, [e.target.name] : e.target.value}))
-	}
-	const handleBirth = (e)=>{
-		setUserData(pv=>({...pv, [e.target.name] : e.target.value.split("-").reverse().join("-")}))
-	}
+function UserInfosForm({setUserData }) {
+	const handleChange = (e) => {
+		setUserData((pv) => ({ ...pv, [e.target.name]: e.target.value }));
+	};
+	const handleBirth = (e) => {
+		let str = e.target.value;
+		const date = new Date(str);
+		const options = { year: "numeric", month: "long", day: "numeric" };
+		setUserData(pv=>({...pv, [e.target.name] : date.toLocaleDateString("fr-FR", options)}))
+	};
 	return (
 		<div>
-			<Field
-				name="Nom"
-				onChange={handleChange}
-			>
+			<Field name="Nom" onChange={handleChange}>
 				Nom
 			</Field>
 
-			<Field
-				name="Prénom"
-				onChange={handleChange}
-			>
+			<Field name="Prénom" onChange={handleChange}>
 				Prénom
 			</Field>
 
-			<BirthDate
-				name="Date de naissance"
-				onChange={handleBirth}
-			>
+			<BirthDate name="Date de naissance" onChange={handleBirth}>
 				Date de naissance
 			</BirthDate>
 
-			<Email
-				name="email"
-				onChange={handleChange}
-			>
+			<Email name="email" onChange={handleChange}>
 				Email
 			</Email>
 
-			<Phone
-				name="Téléphone"
-				onChange={handleChange}
-			>
+			<Phone name="Téléphone" onChange={handleChange}>
 				Téléphone
 			</Phone>
 
-			<GitHub
-				name="Lien Github ou Gitlab"
-				onChange={handleChange}
-			>
+			<GitHub name="Lien Github ou Gitlab" onChange={handleChange}>
 				Lien Github
 			</GitHub>
 		</div>
 	);
 }
-export default UserInfosForm
+export default UserInfosForm;
